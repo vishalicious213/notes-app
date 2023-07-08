2526821f006e0555892ff4b58b21585d74374b2e
vishalicious213 <vishalicious213@gmail.com>
2023-07-08 11:08:23 -0700

    added delete-btn and icon code to Sidebar for each note

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,13 +4,15 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+                <button className="delete-btn">
+                    <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
     ))
