a6172372902fd87793ab9636c15588d45e27e216
vishalicious213 <vishalicious213@gmail.com>
2023-07-08 22:55:53 -0700

    removed conditional rendering of Editor and made it so it always renders after Sidebar. added optional chaining to ReactMde in Editor

--- src/App.jsx
+++ src/App.jsx
@@ -69,14 +69,10 @@ export default function App() {
                     newNote={createNewNote}
                     deleteNote={deleteNote}
                 />
-                {
-                    currentNoteId && 
-                    notes.length > 0 &&
-                    <Editor 
-                        currentNote={currentNote}
-                        updateNote={updateNote} 
-                    />
-                }
+                <Editor 
+                    currentNote={currentNote}
+                    updateNote={updateNote} 
+                />
             </Split>
             :
             <div className="no-notes">
