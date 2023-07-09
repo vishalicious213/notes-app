e3f431116800e6bf0dda3db9174070ec5eaa3a82
vishalicious213 <vishalicious213@gmail.com>
2023-07-08 18:27:56 -0700

    drafted firebase.js for firebase configuration

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyALv3DlEpY4bmxk9KjpY6NZ4u-gvq3dZ6A",
+    authDomain: "react-notes-ba146.firebaseapp.com",
+    projectId: "react-notes-ba146",
+    storageBucket: "react-notes-ba146.appspot.com",
+    messagingSenderId: "376174424992",
+    appId: "1:376174424992:web:e91ebb4da7bef46b602ac5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
