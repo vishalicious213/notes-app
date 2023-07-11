47d561833a9d5e840f4143ff77232774df28f9b9
vishalicious213 <vishalicious213@gmail.com>
2023-07-11 10:22:01 -0700

    created tempNoteText state, to begin setting up debouncing

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ export default function App() {
     const sortedNotes = notes.sort(function(a, b) {
         return b.updatedAt - a.updatedAt
     })
+    const [tempNoteText, setTempNotesText] = React.useState("")
 
     useEffect(() => {
         const unsubscribe = onSnapshot(notesCollection, function(snapshot) {
